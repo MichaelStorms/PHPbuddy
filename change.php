df487c3d440eb1e2a18591d6c5c46ba5e20cfdef
MrCuli <culibrk27@gmail.com>
2020-03-21 14:56:59 +0100

    post works & conected on localhost

--- change.php
+++ change.php
@@ -1,16 +1,30 @@
 <?php
+    include_once(__DIR__ . "/settings/setting.php");
+    $conn = mysqli_connect('mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['db'], SETTINGS['db']['user'], SETTINGS['db']['password']);
+
+    if (!empty($_POST)) {
+        $image = $_POST['image'];
+        var_dump($image);
+    }
 
     
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>change profile</title>
 </head>
+
 <body>
-    
+    <form action="" method="post">
+        <input type="file" name="image">
+        <input type="submit" value="change">
+    </form>
 </body>
+
 </html>
--- settings/setting.php
+++ settings/setting.php
@@ -5,7 +5,7 @@
             "user" => "root",
             "password" => "",
             "host" => "localhost",
-            "port" => 3308,
+            "port" => 3306,
             "db" => "phpbuddy"
         ]
     ];
