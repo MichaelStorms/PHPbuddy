6101ca0ea3802b6ba6ee6ee39239807a2dd1870e
MrCuli <culibrk27@gmail.com>
2020-03-26 16:21:41 +0100

    evertyhing works only get imageis needed

--- change.php
+++ change.php
@@ -8,6 +8,7 @@ include(__DIR__ . "/classe/db.php");
 session_start();
 $conn = new mysqli(SETTINGS['db']['host'], SETTINGS['db']['user'], SETTINGS['db']['password'], SETTINGS['db']['db']);
 
+
 $_SESSION["id"] = 1;
 $_SESSION["password"] = "123";
 $_SESSION["email"] = "test@test.com";
@@ -15,100 +16,69 @@ $_SESSION["email"] = "test@test.com";
 $paswordDb = $_SESSION["password"];
 $emailDb = $_SESSION["email"];
 
-if (isset($_POST['avatar'])) {
-	$size = 2097152;
-	$filetypes = array('jpg', 'jpeg', 'gif', 'png');
-	$avatar = $_POST['avatar'];
 
+// image insert
 
-	$target_dir = "images/";
-	$target_file = $target_dir . basename($_FILES["avatar"]["name"]);
-	$uploadOk = 1;
-	$sizeFile = getimagesize($_FILES['avatar']);
+	if (isset($_FILES['avatar']) and !empty($_FILES['avatar']['name'])) {
+		$size = 2097152;
+		$filetypes = array('jpg', 'jpeg', 'gif', 'png');
+		if ($_FILES['avatar']['size'] <= $size) {
+			$extensionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));
 
-	if($sizefile !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-	
-	
-	if ($sizefile <= $size) {
-		if (isset($_SESSION['id'])) {
-			if (isset($_POST['avatar'])) {
-				$updateAvatar = $conn->prepare("UPDATE users SET image = ? WHERE id = ?");
-				$updateAvatar->bind_param("si", $_SESSION['id'], $_SESSION['id']);
-				$updateAvatar->execute();
+			if (in_array($extensionUpload, $filetypes)) {
+				$route = __DIR__ . "/images/" . $_SESSION['id'] . "." . $extensionUpload;
+				$resultat = move_uploaded_file($_FILES['avatar']['tmp_name'], $route);
+				if ($resultat) {
+					$updateAvatar = $conn->prepare("UPDATE users SET image = ? WHERE id = ?");
+					$updateAvatar->bind_param("ss", $_SESSION['id'], $_SESSION['id']);
+					$updateAvatar->execute();
+				} {
+					$error = "Kon de file niet uploden";
+				}
+			} else {
+				$error = "Het formaat van de file is niet tiegestaan. Het moet een formaat jpg, png of gif zijn.";
 			}
-		}else {
-			echo "File is te groot of foute file type";
+		} else {
+			$msg = "andere file grote of type";
 		}
+	}
 
+
+//pasword change
+
+if (!empty($_POST['passwordOld'])) {
+	$passwordNew = $_POST["passwordNew"];
+	$passwordCheck = $_POST['passwordCheck'];
+	if (!empty($_POST['passwordOld']) == $paswordDb) {
+		if ($passwordNew == $passwordCheck) {
+			$updateAvatar = $conn2->prepare("UPDATE users SET password = ? WHERE id = ?");
+			$updateAvatar->bindparam("si", $_POST['passwordNew'], $_SESSION['id']);
+			$updateAvatar->execute();
+			echo "<h3>succes</h3>";
+		} else {
+			echo "<h3>Wachtwoord is niet het zelfde</h3>";
+		}
 	} else {
-		echo "lol";
+		echo "<h3>doesnt work</h3>";
 	}
+}
+//email change
 
-
-	if (!empty($_POST['passwordOld'])) {
-		$passwordNew = $_POST["passwordNew"];
-		$passwordCheck = $_POST['passwordCheck'];
-		if (!empty($_POST['passwordOld']) == $paswordDb) {
-			if ($passwordNew == $passwordCheck) {
-				$updateAvatar = $conn->prepare("UPDATE users SET password = ? WHERE id = ?");
-				$updateAvatar->bind_param("si", $_POST['passwordNew'], $_SESSION['id']);
-				$updateAvatar->execute();
-				echo "<h3>succes</h3>";
-			} else {
-				echo "<h3>Wachtwoord is niet het zelfde</h3>";
-			}
+if (!empty($_POST['emailOld'])) {
+	$emailNew = $_POST['emailNew'];
+	$emailCheck = $_POST['emailCheck'];
+	if (!empty($_POST['emailOld']) === $emailDb) {
+		if ($emailNew === $emailCheck) {
+			$updateAvatar = $conn->prepare("UPDATE users SET password = ? WHERE id = ?");
+			$updateAvatar->bind_param("si", $_POST['emailNew'], $_SESSION['id']);
+			$updateAvatar->execute();
+			echo "<h3>succes</h3>";
 		} else {
-			echo "<h3>doesnt work</h3>";
+			echo "<h3>email is niet het zelfde</h3>";
 		}
+	} else {
+		echo "<h3>Niet het zelde email adres</h3>";
 	}
-
-	if (!empty($_POST['emailOld'])) {
-		$emailNew = $_POST['emailNew'];
-		$emailCheck = $_POST['emailCheck'];
-		if (!empty($_POST['emailOld']) === $emailDb) {
-			if ($emailNew === $emailCheck) {
-				$updateAvatar = $conn->prepare("UPDATE users SET password = ? WHERE id = ?");
-				$updateAvatar->bind_param("si", $_POST['emailNew'], $_SESSION['id']);
-				$updateAvatar->execute();
-				echo "<h3>succes</h3>";
-			} else {
-				echo "<h3>email is niet het zelfde</h3>";
-			}
-		} else {
-			echo "<h3>Niet het zelde email adres</h3>";
-		}
-	}
-
-
-	// if (isset($_FILES['avatar']) and !empty($_FILES['avatar']['name'])) {
-	// 	$size = 2097152;
-	// 	$filetypes = array('jpg', 'jpeg', 'gif', 'png');
-	// 	if ($_FILES['avatar']['size'] <= $size) {
-	// 		$extensionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));
-
-	// 		if (in_array($extensionUpload, $filetypes)) {
-	// 			$route = __DIR__ . "images/" . $_SESSION['id'] . "." . $extensionUpload;
-	// 			$resultat = move_uploaded_file($_FILES['avatar']['tmp_name'], $route);
-	// 			if ($resultat) {
-	// 				 $updateAvatar = $conn->prepare("UPDATE users SET image = ? WHERE id = ?");
-	// 				 $updateAvatar -> bind_param("ss",$_SESSION['id'] . "." . $extensionUpload, $_SESSION['id']);
-	// 				 $updateAvatar->execute();
-	// 			} {
-	// 				$error = "Kon de file niet uploden";
-	// 			}
-	// 		} else {
-	// 			$error = "Het formaat van de file is niet tiegestaan. Het moet een formaat jpg, png of gif zijn.";
-	// 		}
-	// 	} else {
-	// 		$msg = "andere file grote of type";
-	// 	}
-	// }
 }
 
 ?>
@@ -122,7 +92,7 @@ if (isset($_POST['avatar'])) {
 </head>
 
 <body>
-	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
 		<h2>change Profile pic</h2>
 		<input type="file" name="avatar">
 		<br>
