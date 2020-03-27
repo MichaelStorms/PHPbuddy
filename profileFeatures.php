3c7fcbec51b3dac4551250e4d43942eb01214d18
Lisa_Drse <55496129+r0423168@users.noreply.github.com>
2020-03-27 15:27:19 +0100

    edited title
    Wrong title name in HTML, changed to Profile Settings after review team members

--- profileFeatures.php
+++ profileFeatures.php
@@ -50,7 +50,7 @@
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">	
-		<title><?php echo $user['username'] ?>'s Profile Settings</title>
+		<title>Profile Settings</title>
     </head> 
 	<body style="margin-left:20px;">      
 		<h3>Update Profile Information</h3> 
