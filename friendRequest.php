c7a6b209067557b030f9b3c2c8cc6d1febdef5db
Lisa_Drse <55496129+r0423168@users.noreply.github.com>
2020-04-03 14:43:07 +0200

    file friendrequest
    added friendrequest.php for basis buddierequests

---
+++ friendRequest.php
@@ -0,0 +1,83 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+  <div class="top">
+    <h2>Friend's List</h2>
+  </div>
+  <div class="row">
+
+    <div class="shadow">
+      <div class="col-sm-12">
+        <div class="col-sm-2">
+          <img src='https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight&accessoriesType=Blank&hairColor=BlondeGolden&facialHairType=Blank&clotheType=ShirtVNeck&clotheColor=PastelBlue&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Pale' class="img-circle" width="60px">
+        </div>
+        <div class="col-sm-8">
+          <h4><a href="#">Friend 1</a></h4>
+          <p><a href="#">4 mutual friends</a></p>
+        </div>
+        <div class="col-sm-2">
+          <br>
+          <a href="#">Send Request</a>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+      <hr />
+
+      <div class="col-sm-12">
+        <div class="col-sm-2">
+          <img src='https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortWaved&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=Gray01&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Pale' class="img-circle" width="60px">
+        </div>
+        <div class="col-sm-8">
+          <h4><a href="#">Friend 2</a></h4>
+          <p><a href="#">4 mutual friends</a></p>
+        </div>
+        <div class="col-sm-2">
+          <br>
+          <a href="#">Send Request</a>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+      <hr />
+
+      <div class="col-sm-12">
+        <div class="col-sm-2">
+          <img src='https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Wayfarers&hairColor=Auburn&facialHairType=Blank&clotheType=CollarSweater&clotheColor=Heather&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light' class="img-circle" width="60px">
+        </div>
+        <div class="col-sm-8">
+          <h4><a href="#">Friend 3</a></h4>
+          <p><a href="#">4 mutual friends</a></p>
+        </div>
+        <div class="col-sm-2">
+          <br>
+          <a href="#">Send Request</a>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+      <hr />
+      
+      <div class="col-sm-12">
+        <div class="col-sm-2">
+          <img src='https://avataaars.io/?avatarStyle=Transparent&topType=LongHairCurvy&accessoriesType=Prescription02&hairColor=Blonde&facialHairType=Blank&clotheType=ShirtVNeck&clotheColor=Blue03&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light' class="img-circle" width="60px">
+        </div>
+        <div class="col-sm-8">
+          <h4><a href="#">Friend 4</a></h4>
+          <p><a href="#">4 mutual friends</a></p>
+        </div>
+        <div class="col-sm-2">
+          <br>
+          <a href="#">Send Request</a>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
