d0776a9f01c2ddd67a92d638820ed6de0b48b4bb
Lisa_Drse <55496129+r0423168@users.noreply.github.com>
2020-04-06 10:41:54 +0200

    Delete header.php

--- header.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Welcome to PHP Buddy!</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
