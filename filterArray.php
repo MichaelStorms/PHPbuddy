8bf718b8d91cafce01b933d806aad1d76a6ed68c
MichaelStorms <49272397+MichaelStorms@users.noreply.github.com>
2020-04-14 14:48:39 +0200

    missing files

---
+++ filterArray.php
@@ -0,0 +1,19 @@
+<?php
+    $coursesList = [
+        0 => "Development",
+        1 => "Design",
+        2 => "Design & Development"
+    ];
+
+    $hobbyList = [
+        0 => "Voetbal",
+        1 => "Basketbal",
+        2 => "Gaming",
+        3 => "Films kijken"
+    ];
+
+    $extraList = [
+        0 => "foodie",
+        1 => "party"
+    ]
+?>
