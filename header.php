d12c0f7afc9f78bca00b8b0b1874bfb7cf7d0e67
MichaelStorms <49272397+MichaelStorms@users.noreply.github.com>
2020-04-27 12:49:54 +0200

    databank

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Welcome to PHP Buddy!</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
