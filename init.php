46fb2352651dc173427c87dffff08c4f2a753078
MichaelStorms <49272397+MichaelStorms@users.noreply.github.com>
2020-04-27 12:50:38 +0200

    feature 18 v1.0 + update databank

--- init.php
+++ init.php
@@ -5,13 +5,17 @@ session_regenerate_id(true);
 include('classes/db.php');
 include('classes/user.php');
 include('classes/friends.php');
+include('classes/Chat.php');
 
 // DATABASE CONNECTIONS
 /*$conn = new Db();
 $db_connection = $conn->getConnection();
 */
 // USER OBJECT
-$user = new User();//$db_connection);
+$user = new User();
 // FRIEND OBJECT
-$buddy = new Buddy();//$db_connection);
+$buddy = new Buddy();
+// CHAT OBJECT
+$chat = new Chat();
+
 ?>
--- phpbuddy.sql
+++ phpbuddy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Gegenereerd op: 20 apr 2020 om 14:51
+-- Gegenereerd op: 27 apr 2020 om 10:49
 -- Serverversie: 8.0.18
 -- PHP-versie: 7.3.12
 
@@ -32,11 +32,47 @@ DROP TABLE IF EXISTS `buddychat`;
 CREATE TABLE IF NOT EXISTS `buddychat` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `sender_id` int(11) NOT NULL,
-  `receiver_id` int(11) NOT NULL,
+  `reciever_id` int(11) NOT NULL,
   `message` varchar(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `messageread` tinyint(1) DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `buddychat`
+--
+
+INSERT INTO `buddychat` (`id`, `sender_id`, `reciever_id`, `message`, `timestamp`, `status`) VALUES
+(1, 5, 0, 'hello', '2020-04-25 14:54:30', 1),
+(2, 5, 7, 'Hello there', '2020-04-25 14:55:43', 0),
+(3, 7, 5, 'hello', '2020-04-25 14:58:19', 1),
+(4, 7, 9, 'hello', '2020-04-25 15:16:55', 1),
+(5, 7, 6, 'hey', '2020-04-25 15:48:13', 1),
+(6, 7, 6, 'hello', '2020-04-25 15:49:29', 1),
+(7, 7, 6, 'hey tehre', '2020-04-25 15:49:39', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `chat_login_details`
+--
+
+DROP TABLE IF EXISTS `chat_login_details`;
+CREATE TABLE IF NOT EXISTS `chat_login_details` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `last_activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_typing` enum('no','yes') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `chat_login_details`
+--
+
+INSERT INTO `chat_login_details` (`id`, `userid`, `last_activity`, `is_typing`) VALUES
+(0, 5, '2020-04-25 14:08:53', 'no');
 
 -- --------------------------------------------------------
 
@@ -53,7 +89,15 @@ CREATE TABLE IF NOT EXISTS `friends` (
   PRIMARY KEY (`id`),
   KEY `user_one` (`user_one`),
   KEY `user_two` (`user_two`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `friends`
+--
+
+INSERT INTO `friends` (`id`, `user_one`, `user_two`, `buddies`) VALUES
+(1, 7, 5, 0),
+(2, 5, 6, 0);
 
 -- --------------------------------------------------------
 
@@ -69,7 +113,15 @@ CREATE TABLE IF NOT EXISTS `friend_request` (
   PRIMARY KEY (`id`),
   KEY `sender` (`sender`),
   KEY `receiver` (`receiver`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `friend_request`
+--
+
+INSERT INTO `friend_request` (`id`, `sender`, `receiver`) VALUES
+(3, 5, 10),
+(5, 5, 8);
 
 -- --------------------------------------------------------
 
@@ -86,7 +138,7 @@ CREATE TABLE IF NOT EXISTS `notifications` (
   `status` text NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `notifications`
@@ -107,7 +159,11 @@ INSERT INTO `notifications` (`id`, `name`, `type`, `message`, `status`, `date`) 
 (21, '', 'comment', 'hello', 'read', '2020-04-07 13:15:50'),
 (22, '', 'comment', 'hello', 'unread', '2020-04-07 13:15:50'),
 (23, 'lkdskfhsj', 'like', '', 'read', '2020-04-07 13:47:42'),
-(24, 'lkdskfhsj', 'like', '', 'unread', '2020-04-07 13:47:42');
+(24, 'lkdskfhsj', 'like', '', 'read', '2020-04-07 13:47:42'),
+(25, '', 'comment', 'hello', 'read', '2020-04-23 14:32:09'),
+(26, '', 'comment', 'hello', 'unread', '2020-04-23 14:32:09'),
+(27, '', 'comment', 'hello', 'unread', '2020-04-23 14:32:13'),
+(28, '', 'comment', 'hello', 'unread', '2020-04-23 14:32:13');
 
 -- --------------------------------------------------------
 
@@ -130,6 +186,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `buddy` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `image` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `imgDescription` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `current_session` int(11) NOT NULL,
+  `online` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -137,13 +195,13 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Gegevens worden geëxporteerd voor tabel `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `locatie`, `interests`, `hobby`, `extra`, `class`, `buddy`, `image`, `imgDescription`) VALUES
-(5, 'Michaël', 'Storms', 'r0701956@student.thomasmore.be', '$2y$16$nJjC5dZwz1xCOG1G.L40muhAeE/jqu3PJIADHz7ps4k96zt70Gp9C', 'malle', 'Design', 'Gaming', 'foodie', '3IMD', 'BuddySearcher', '', ''),
-(6, 'john', 'doe', 'r0701757@student.thomasmore.be', '$2y$16$75Ec4jiBwkvXkjl.jcuU6Ox35MP0My78oeDMNUk0ZsXjEkY13Tuti', 'Mechelen', 'Development', 'Voetbal', 'party', '1IMD', 'BuddySearcher', '', ''),
-(7, 'Nina', 'Van der kerken', 'r0701958@student.thomasmore.be', '$2y$16$fPRZivdzT7wCU7KdYBGVAeL6Ao3b51Mvd8NxWlJR3d8BkO24vx0Bm', '', '', '', '', '', 'BuddySearcher', '', ''),
-(8, 'Kevin', 'De Vos', 'r0701959@student.thomasmore.be', '$2y$16$bl1XVyZOGlS7asMaRvNAxuof5A7pod1SFZ/u5C3XvgAc/lCAaDipm', 'malle', 'Design & Development', 'Basketbal', 'foodie', '3IMD', 'BuddyHolder', '', ''),
-(9, 'Jasmina', 'Mulder', 'r0701916@student.thomasmore.be', '$2y$16$LROOy5giaT0u40rvL0J14.fSJeiffkNWStxdaaWCGx3e9PKLxqRVC', 'Lier', 'Design & Development', 'Gaming', 'foodie', '1IMD', 'BuddySearcher', '', ''),
-(10, 'luka', 'luka', 'r0764418@student.thomasmore.be', '$2y$16$wuhQlZ8jMi/bByz9vw8IIuJJiEEybphTaDlp7iL.RZDp.Hu7aU19u', '', '', '', '', '', '', '', '');
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `locatie`, `interests`, `hobby`, `extra`, `class`, `buddy`, `image`, `imgDescription`, `current_session`, `online`) VALUES
+(5, 'Michaël', 'Storms', 'r0701956@student.thomasmore.be', '$2y$16$nJjC5dZwz1xCOG1G.L40muhAeE/jqu3PJIADHz7ps4k96zt70Gp9C', 'malle', 'Design', 'Gaming', 'foodie', '3IMD', 'BuddySearcher', '', '', 7, 0),
+(6, 'john', 'doe', 'r0701757@student.thomasmore.be', '$2y$16$75Ec4jiBwkvXkjl.jcuU6Ox35MP0My78oeDMNUk0ZsXjEkY13Tuti', 'Mechelen', 'Development', 'Voetbal', 'party', '1IMD', 'BuddySearcher', '', '', 0, 0),
+(7, 'Nina', 'Van der kerken', 'r0701958@student.thomasmore.be', '$2y$16$fPRZivdzT7wCU7KdYBGVAeL6Ao3b51Mvd8NxWlJR3d8BkO24vx0Bm', 'malle', 'Development', 'Voetbal', 'foodie', '1IMD', 'BuddySearcher', '', '', 6, 1),
+(8, 'Kevin', 'De Vos', 'r0701959@student.thomasmore.be', '$2y$16$bl1XVyZOGlS7asMaRvNAxuof5A7pod1SFZ/u5C3XvgAc/lCAaDipm', 'malle', 'Design & Development', 'Basketbal', 'foodie', '3IMD', 'BuddyHolder', '', '', 0, 0),
+(9, 'Jasmina', 'Mulder', 'r0701916@student.thomasmore.be', '$2y$16$LROOy5giaT0u40rvL0J14.fSJeiffkNWStxdaaWCGx3e9PKLxqRVC', 'Lier', 'Design & Development', 'Gaming', 'foodie', '1IMD', 'BuddySearcher', '', '', 0, 0),
+(10, 'luka', 'luka', 'r0764418@student.thomasmore.be', '$2y$16$wuhQlZ8jMi/bByz9vw8IIuJJiEEybphTaDlp7iL.RZDp.Hu7aU19u', '', '', '', '', '', '', '', '', 0, 0);
 
 --
 -- Beperkingen voor geëxporteerde tabellen
